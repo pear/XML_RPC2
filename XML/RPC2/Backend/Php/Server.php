b3432577a1c649f7d59efa5db1bfd67097bb1539
Fabien MARTY <fab@php.net>
2006-01-17 22:26:07 +0000

    split handleCall() in two parts (better for debug)
    git-svn-id: http://svn.php.net/repository/pear/packages/XML_RPC2/trunk@205485 c90b9560-bf6c-de11-be94-00142212c4b1

--- XML/RPC2/Backend/Php/Server.php
+++ XML/RPC2/Backend/Php/Server.php
@@ -91,41 +91,51 @@ class XML_RPC2_Backend_Php_Server extends XML_RPC2_Server
     public function handleCall()
     {
         if ((!($this->autoDocument)) or ((isset($GLOBALS['HTTP_RAW_POST_DATA'])) && (strlen($GLOBALS['HTTP_RAW_POST_DATA'])>0))) {
-            try {
-                $oldErrorHandler = set_error_handler(array('XML_RPC2_Backend_Php_Server', 'errorToException'));
-                $request = @simplexml_load_string($GLOBALS['HTTP_RAW_POST_DATA']);
-                if (!is_object($request)) throw new XML_RPC2_FaultException('Unable to parse request XML', 0);
-                $request = XML_RPC2_Backend_Php_Request::createFromDecode($request);  
-                $methodName = $request->getMethodName();
-                if (array_key_exists($methodName, $this->getAliases())) {
-                    $methodName = $this->aliases[$method];
-                }
-                $arguments = $request->getParameters();
-                if ($this->signatureChecking) {
-                    $method = $this->callHandler->getMethod($methodName);
-                    if (!($method)) {
-                        // see http://xmlrpc-epi.sourceforge.net/specs/rfc.fault_codes.php for standard error codes 
-                        print(XML_RPC2_Backend_Php_Response::encodeFault(-32601, 'server error. requested method not found'));
-                        die();
-                    }
-                    if (!($method->matchesSignature($methodName, $arguments))) {
-                        print(XML_RPC2_Backend_Php_Response::encodeFault(-32602, 'server error. invalid method parameters'));		
-                        die();
-                    }
-                }
-                print(XML_RPC2_Backend_Php_Response::encode(call_user_func_array(array($this->callHandler, $methodName), $arguments)));
-                if ($oldErrorHandler !== FALSE) set_error_handler($oldErrorHandler);
-            } catch (XML_RPC2_FaultException $e) {
-                print(XML_RPC2_Backend_Php_Response::encodeFault($e->getFaultCode(), $e->getMessage()));
-            } catch (Exception $e) {
-                print(XML_RPC2_Backend_Php_Response::encodeFault(1, 'Unhandled ' . get_class($e) . ' exception:' . $e->getMessage()));
-            }
+            $response = $this->getResponse();
+            header('Content-type: text/xml; charset=' . $this->encoding);
+            header('Content-length: '.strlen($response));
+            print $response;
         } else {
             $this->autoDocument();
         }
     }
     
     // }}}
+    // {{{ getResponse()
+    
+    /**
+     * Get the XML response of the XMLRPC server
+     * 
+     * @return string XML response
+     */
+    public function getResponse()
+    {
+        try {
+            $oldErrorHandler = set_error_handler(array('XML_RPC2_Backend_Php_Server', 'errorToException'));
+            $request = @simplexml_load_string($GLOBALS['HTTP_RAW_POST_DATA']);
+            // TODO : do not use exception but a XMLRPC error !
+            if (!is_object($request)) throw new XML_RPC2_FaultException('Unable to parse request XML', 0);
+            $request = XML_RPC2_Backend_Php_Request::createFromDecode($request);  
+            $methodName = $request->getMethodName();
+            $arguments = $request->getParameters();
+            if ($this->signatureChecking) {
+                $method = $this->callHandler->getMethod($methodName);
+                if (!($method)) {
+                    // see http://xmlrpc-epi.sourceforge.net/specs/rfc.fault_codes.php for standard error codes 
+                    return (XML_RPC2_Backend_Php_Response::encodeFault(-32601, 'server error. requested method not found'));
+                }
+                if (!($method->matchesSignature($methodName, $arguments))) {
+                    return (XML_RPC2_Backend_Php_Response::encodeFault(-32602, 'server error. invalid method parameters'));		
+                }
+            }
+            if ($oldErrorHandler !== FALSE) set_error_handler($oldErrorHandler);
+            return (XML_RPC2_Backend_Php_Response::encode(call_user_func_array(array($this->callHandler, $methodName), $arguments)));
+        } catch (XML_RPC2_FaultException $e) {
+            return (XML_RPC2_Backend_Php_Response::encodeFault($e->getFaultCode(), $e->getMessage()));
+        } catch (Exception $e) {
+            return (XML_RPC2_Backend_Php_Response::encodeFault(1, 'Unhandled ' . get_class($e) . ' exception:' . $e->getMessage()));
+        }        
+    }
     
 }
 ?>
