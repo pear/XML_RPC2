f31569d11e97653a32aa7f2cd3e81c918db22d69
Nick Burka <nick@silverorange.com>
2013-01-16 23:01:13 -0400

    Respect the error_reporting ini setting so that the full details of the error code including stack-trace isn't displayed publicly on a live site. Handle just default php Exceptions in the try/catch blocks of the server backends.

--- XML/RPC2/Backend/Php/Server.php
+++ XML/RPC2/Backend/Php/Server.php
@@ -136,8 +136,12 @@ class XML_RPC2_Backend_Php_Server extends XML_RPC2_Server
         } catch (XML_RPC2_FaultException $e) {
             return (XML_RPC2_Backend_Php_Response::encodeFault($e->getFaultCode(), $e->getMessage(), $this->encoding));
         } catch (Exception $e) {
-            return (XML_RPC2_Backend_Php_Response::encodeFault(1, 'Unhandled ' . get_class($e) . ' exception:' . $e->getMessage() . $e->getTraceAsString(), $this->encoding));
-        }        
+            if (ini_get('display_errors') == 1) {
+                return (XML_RPC2_Backend_Php_Response::encodeFault(1, 'Unhandled ' . get_class($e) . ' exception:' . $e->getMessage() . $e->getTraceAsString(), $this->encoding));
+            } else {
+                return XML_RPC2_Backend_Php_Response::encodeFault(1, '', $this->encoding);
+            }
+        }
     }
     
 }
